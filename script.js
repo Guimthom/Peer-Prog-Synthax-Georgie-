3a9a036af3b2907015c135d37bcbdc37993d4f72
Pixel Gui <guillaumethomart@gmail.com>
2024-02-02 15:59:13 +0100

    Exercice finalisé sur les conditions

--- script.js
+++ script.js
@@ -3,68 +3,53 @@
 const age = Math.floor(Math.random() * 50);
 
 // [1] créer une variable name, et lui assigner un nom
-let name = "Georgi";
+let name = "Georgie";
 
 // [2] si age est plus grand que 18, afficher le message suivant
 const msg1 = 'Tiens voila ta bière car tu as ' + age + 'ans';
-if (age >= 18) {
+if (age > 18) {
     console.log(msg1);
 }else{
   console.log("Déso t'as " + age + "ans")
 }
 
-/* Inverse la condition /
-// if (!(age > 18)) {
-//     console.log(msg1);
-// }
+// Inverse la condition 
+ if (!(age > 18)) {
+    console.log(msg1);
+ }
 
 // [3] si age n'est pas entre 12 et 25, afficher le message suivant:
 const msg2 = "Vous n'avez pas le droit à la carte 12-25";
-if (age < 12 || age > 25) {
-    console.log(msg2);
+if ( age < 12 || age > 25){
+console.log(msg2 + "car tu as " + age + "ans")
+}else{
+console.log("c'est bon t'as la carte")
 }
 
 
 // [4] si age est plus grand que 20, réassigner name en lui ajoutant 'Mr' (ou 'Mme') devant
 if (age > 20) {
-name = 'Mme' + name
+name = 'Mme ' + name;
     console.log(name);
+}else{
+  console.log("déso petite " + name)
 }
 
+// Besoin d'instructions
 
-// OU 
-const ageNew = 30;
-
-// [1] créer une variable name, et lui assigner un nom
 const person = {
   age: 22,
   name: "Toto",
-  genre: "licorne"
+  genre: "Licorne"
 }
+console.log(person);
 
-// let name = "Georgi";
-
-// [2] si age est plus grand que 18, afficher le message suivant
-const msg1New = 'Bravo pour le permis';
-if (person.ageNew > 18) {
-  console.log(msg1New);
-}
-/ Inverse la condition */
-// if (!(age > 18)) {
-//     console.log(msg1);
-// }
-
-// [3] si age n'est pas entre 12 et 25, afficher le message suivant:
-/* const msg2New = "Vous n'avez pas le droit à la carte 12-25";
-if (person.ageNew < 12 || age > 25) {
-  console.log(msg2New);
-} */
-
-// [4] si age est plus grand que 20, réassigner name en lui ajoutant 'Mr' (ou 'Mme') devant
-/* if (person.genre === 'Licorne') {
-  person.name = Mr $(person.name)
+ if (person.genre === 'Licorne') {
+  person.name = `Mr ${person.name}`;
+  
 } else if (person.genre === 'Female') {
-  person.name = Mr $(person.name)
+  person.name = `Mlle ${person.name}`;
+  console.log(person.name);
 } else {
-  person.name = Undefined $(person.name)
-} */
+  person.name = `Undefined ${person.name}`;
+} 
